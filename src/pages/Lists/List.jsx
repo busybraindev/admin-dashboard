42d1a7809c4d4d42bfc96b6804eb75b3b46e6909
Mubarak Adeyemi <busybraindev@gmail.com>
2026-07-09 19:19:59 +0100

    Fix

--- src/pages/Lists/List.jsx
+++ src/pages/Lists/List.jsx
@@ -41,7 +41,7 @@ const List = ({ url }) => {
         {lt.map((item, i) => {
           return (
             <div key={i} className="ltbft">
-              <img src={`${url}/images/` + item.image} alt="" />
+              <img src={item.image} alt="" />
               <p>{item.name}</p>
               <p>{item.category}</p>
               <p>${item.price}</p>
